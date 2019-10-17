c9cb9927b88c26267e099bc1b2e797ee0bf9adf7
1605204 <teodor.narliyski@gmail.com>
2019-10-17 11:57:49 +0100

    Added all screens. Wireframes yet to be done (except for the Login scren).

---
+++ app/src/main/java/uk/ac/rgu/cwpartone/ProductsList.java
@@ -0,0 +1,14 @@
+package uk.ac.rgu.cwpartone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProductsList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_products_list);
+    }
+}
