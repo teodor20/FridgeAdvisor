aec008bec4eb2ef2be2f3101ca042f2d5c4e8c3c
teodor.narliyski <teodor.narliyski@gmail.com>
2019-12-05 21:27:24 +0000

    Main page completed - the retrieved products are now being saved into the shared prefs Note: We might need to delete some toasts/text views before the end but they are all clearly visible so we won't forget.

--- app/src/main/java/uk/ac/rgu/cwpartone/ScanActivity.java
+++ app/src/main/java/uk/ac/rgu/cwpartone/ScanActivity.java
@@ -1,27 +1,58 @@
 package uk.ac.rgu.cwpartone;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.zxing.Result;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Calendar;
+import java.util.Map;
+import java.util.Set;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
-public class ScanActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+public class ScanActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler, DatePickerDialog.OnDateSetListener {
     ZXingScannerView scannerView;
+    private String url="https://api.barcodelookup.com/v2/products?barcode=";
+    private String APIKey="&formatted=y&key=zvbfvvqjv1r09fpxoqrj79o1itv8pg";
+    private String date;
+    private String productName;
+    private final String sharedPrefFile = "uk.ac.rgu.cwpartone.demo";
+    private SharedPreferences sharedPrefs;
+    private SharedPreferences.Editor prefsEditor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         scannerView = new ZXingScannerView(this);
         setContentView(scannerView);
+        sharedPrefs = getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
+        prefsEditor = sharedPrefs.edit();
     }
 
     @Override
     public void handleResult(Result result) {
-        MainActivity.resultView.setText(result.getText());
-        onBackPressed();
+        this.getProduct(result.getText());
     }
 
     @Override
@@ -35,5 +66,104 @@ public class ScanActivity extends AppCompatActivity implements ZXingScannerView.
     protected void onPause() {
         super.onPause();
         scannerView.stopCamera();
+    }
+
+    protected void getProduct(String barcode) {
+        MainActivity.resultView.setText(barcode);
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String EndPoint = this.getUrl()+barcode+this.getAPIKey();
+        JsonObjectRequest objectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                EndPoint,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.e("Rest Response", response.toString());
+                        try {
+                            setProductName(response);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        showDatePickerDialog();
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Rest Response", error.toString());
+                        Context context = getApplicationContext();
+                        Toast toast = Toast.makeText(context, R.string.API_Error, Toast.LENGTH_LONG);
+                        onBackPressed();
+                        toast.show();
+                    }
+                }
+        );
+
+        requestQueue.add(objectRequest);
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getAPIKey() {
+        return APIKey;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void showDatePickerDialog() {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+              this,
+                this,
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+        );
+        datePickerDialog.show();
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        setDate(dayOfMonth + "/" + month + "/" + year);
+        saveData(getProductName(), getDate());
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public void setProductName(JSONObject response) throws JSONException {
+        JSONArray arr = response.getJSONArray("products");
+        JSONObject jObj = arr.getJSONObject(0);
+        String productName = jObj.getString("product_name");
+        setProductName(productName);
+    }
+
+    public void saveData(String title, String date) {
+        String item = title + " | " + date;
+        int insertIndex = sharedPrefs.getAll().size();
+        String prefKey = String.valueOf(insertIndex);
+        prefsEditor.putString(prefKey, item);
+        prefsEditor.apply();
+        Context context = getApplicationContext();
+        //Toast toast = Toast.makeText(context, R.string.API_Success, Toast.LENGTH_LONG);
+        Toast toast = Toast.makeText(context, insertIndex + " " + title + " " + date, Toast.LENGTH_LONG);
+        onBackPressed();
+        toast.show();
     }
 }
